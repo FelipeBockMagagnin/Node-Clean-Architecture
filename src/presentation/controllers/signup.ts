8ae513eb3323902096ce0a19720c4cc7309c75d7
felipe.magagnin <felipebmagagnin@gmail.com>
2022-03-03 15:30:48 -0300

    feat: add http request interface

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,5 +1,7 @@
+import { HttpRequest, httpResponse } from '../protocols/http'
+
 export class SignUpController {
-  handle (httpRequest: any): any {
+  handle (httpRequest: HttpRequest): httpResponse {
     return {
       statusCode: 400,
       body: new Error('Missing param: name')
